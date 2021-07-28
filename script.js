eddb66f6ca01bbd5c94f66dc32621d8b4f6a02d5
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-29 00:26:45 +0100

    Create add/remove function to body

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
 const signinBtn = document.querySelector('.signinBtn'),
   signupBtn = document.querySelector('.signupBtn'),
-  formBx = document.querySelector('.formBx');
+  formBx = document.querySelector('.formBx'),
+  body = document.querySelector('body');
 
 signupBtn.onclick = function () {
   formBx.classList.add('active');
+  body.classList.add('active');
 };
 
 signinBtn.onclick = function () {
   formBx.classList.remove('active');
+  body.classList.remove('active');
 };
