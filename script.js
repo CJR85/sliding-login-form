d9ccb502ac865a87064557e4bd54a505d5993af3
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-29 00:24:11 +0100

    Create remove function on formBx

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-const siginBtn = document.querySelector('.siginBtn'),
-  signupBtn = document.querySelector('.sigupBtn'),
+const signinBtn = document.querySelector('.signinBtn'),
+  signupBtn = document.querySelector('.signupBtn'),
   formBx = document.querySelector('.formBx');
 
 signupBtn.onclick = function () {
   formBx.classList.add('active');
+};
+
+signinBtn.onclick = function () {
+  formBx.classList.remove('active');
 };
