a70b26220867332a880b421a81a0608599fe3b58
Calvin Roberts <roberts.calvin85@gmail.com>
2021-07-29 00:18:48 +0100

    Create function for signupBtn

---
+++ script.js
@@ -0,0 +1,7 @@
+const siginBtn = document.querySelector('.siginBtn'),
+  signupBtn = document.querySelector('.sigupBtn'),
+  formBx = document.querySelector('.formBx');
+
+signupBtn.onclick = function () {
+  formBx.classList.add('active');
+};
